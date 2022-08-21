2c6d9942e34dd68dfdbb8ae1cdbced5de4b38677
sam-dev97 <vivatecnosams@gmail.com>
2022-08-20 21:12:33 -0300

    Comparativo comprehension vs for

---
+++ listcomp_vs_fo.py
@@ -0,0 +1,12 @@
+# Usando list comprehension
+lista = [
+        x for x in range(100)
+]
+print(lista)
+
+# Utilizando FOR
+lista2 = []
+for x in range(100):
+        lista2.append(x)
+
+print(lista2)
